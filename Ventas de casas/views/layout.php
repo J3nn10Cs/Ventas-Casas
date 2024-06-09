484099abbd61d4113658e663b7fdafa0ac7df1c3
Jennifer <jennifer15aries@gmail.con>
2024-06-08 20:32:42 -0500

    Ultimas actualizaciones

--- Ventas de casas/views/layout.php
+++ Ventas de casas/views/layout.php
@@ -58,10 +58,10 @@
     <footer class="footer seccion">
         <div class="contenedor contenedor-footer">
             <nav class="navegacion">
-                <a href="/nosotros.php">Nosotros</a>
-                <a href="/anuncios.php">Anuncios</a>
-                <a href="/blog.php">Blog</a>
-                <a href="/contacto.php">Contacto</a>
+                <a href="/nosotros">Nosotros</a>
+                <a href="/propiedades">Anuncios</a>
+                <a href="/blog">Blog</a>
+                <a href="/contacto">Contacto</a>
             </nav>
         </div>
         <p class="oie">Todos los derechos Reservados <?php echo date('Y') ;?> &copy;</p>
